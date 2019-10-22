c584b5138f0a764b0817d5ee2ef37d114abbac6d
Reno DuBois <rdubois96@gmail.com>
2019-10-21 22:08:43 -0500

    Parse results, output to JSON blob

--- parser.js
+++ parser.js
@@ -2,19 +2,20 @@ const fs = require("fs");
 // TODO: update this library to use my own fork, at least until we get a version bump on the main package
 const pdf = require("pdf-parse");
 
-const dataBuffer = fs.readFileSync("./election_results.pdf");
-
 function loadAndParsePDF(pathToPDF) {
-	return new Promise((resolve, reject) => {
+	return new Promise(async (resolve, reject) => {
 		// TODO: handle if the parsing fails
 		const dataBuffer = fs.readFileSync(pathToPDF);
-		pdf(dataBuffer).then((data) => {
+		try {
+			const data = await pdf(dataBuffer);
 			const results = data.text.split("\n");
 			return resolve(results.filter((val) => val.trim()));
-		});
+		} catch (err) {
+			return reject(err);
+		}
 	});
 }
 
 module.exports = {
 	loadAndParsePDF
-}
+};

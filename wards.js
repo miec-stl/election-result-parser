316be8e40fc1c8cb4f855c5c777585bd4eb9e7ed
Mitch Eagles <mitch.eagles@lessannoyingcrm.com>
2019-10-24 18:28:15 -0500

    Left column and right column data

--- wards.js
+++ wards.js
@@ -1,33 +1,58 @@
 const { loadAndParsePDF } = require("./parser");
 
 const StartOfPageString = "Statement of Votes Cast";
+const StartOfNewRaceString = "CITYWIDE";
 
 const RemoveHeaders = (RawFileData) => {
-    const ElectionData = [[]];
+    const ReturnPages = [];
+    const LeftColumns = [];
+    const RightColumns = [];
+    const Races = [];
 
-    let ContinueSkipping = true;
-    let ThisPage = 0;
+    let SkippingHeader = true;
+    let IndexOnPage = 0;
+    let RaceNumber = -1;
+    let CurrentPrecinct;
+    let ThisPageLeftColumn = [];
+    let ThisPageRightColumn = [];
+    let ThisRaceInfo = [];
 
     for (let i = 0; i < RawFileData.length; i++) {
         const Row = RawFileData[i];
         
         if (Row.substring(0, 5) == 'Page:') {
-            ContinueSkipping = false;
-            ThisPage++;
-            ElectionData.push([]);
+            SkippingHeader = false;
+            ThisPageLeftColumn = [];
+            ThisPageRightColumn = [];
             continue;
         } else if (Row == StartOfPageString) {
-            ContinueSkipping = true;
+            ThisPageRightColumn.splice(0, ThisPageLeftColumn.length - ThisPageRightColumn.length);
+            ReturnPages.push({LeftColumn:ThisPageLeftColumn, RightColumn:ThisPageRightColumn, LL:ThisPageLeftColumn.length, RR:ThisPageRightColumn.length});
+            SkippingHeader = true;
+        } else if (Row == StartOfNewRaceString) {
+            // We're looking at a new race
+            RaceNumber++;
+            Races.push(ThisRaceInfo);
+            ThisRaceInfo = [];
         }
 
-        if (ContinueSkipping) {
+        if (SkippingHeader) {
             continue;
         } else {
-            ElectionData[ThisPage].push(Row);
+            if (Row.substring(0,4) == "    ") {
+                // We're in the left column
+                if(Row.substring(0,6) == "    W ") {
+                    CurrentPrecinct = Row.trim();
+                }
+                ThisPageLeftColumn.push(Row.trim());
+            } else {
+                ThisPageRightColumn.push(Row);
+            }
+            // ElectionData.push(Row);
         }        
     }
 
-    return ElectionData;
+    return ReturnPages;
 }
 
 (async () => {

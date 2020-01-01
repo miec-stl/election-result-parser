d6506167abe876534dd657ce8819fcb877cecdc0
Reno DuBois <rdubois96@gmail.com>
2020-01-01 17:35:53 -0600

    Update types

--- src/types.ts
+++ src/types.ts
@@ -1,27 +1,38 @@
 export interface ReturnData {
 	totalVotes?: number;
-	results?: ContestResultsCollection;
+	results: ContestResultsCollection;
 }
 
-export interface ContestResultsCollection {
+interface ContestResultsCollection {
 	[contestName: string]: ContestResult;
 }
 
-export interface OptionDataCollection {
+interface OptionDataCollection {
 	[optionName: string]: OptionData;
 }
 
-export interface WardResultsCollection {
+interface WardResultsCollection {
 	[wardNumber: string]: OptionDataCollection;
 }
 
 export interface ContestResult {
 	totalVotes?: number;
-	results?: OptionDataCollection;
-	wardResults?: WardResultsCollection;
+	timesCounted?: number;
+	timesCountedPercentage?: string;
+	candidates: string[];
+	results: OptionDataCollection;
+	wardResults: WardResultsCollection;
 }
 
 export interface OptionData {
 	numVotes: number;
 	percentageVotes: string;
+}
+
+/**
+ * Types used in the program, but not the return data itself
+ */
+
+export interface ParsedWardResults {
+	[contestName: string]: ContestResult;
 }
